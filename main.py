89e58038b2d4d437e1c9cb48b1e863a92ae5c60e
0lenaIva <88824086+0lenaIva@users.noreply.github.com>
2026-03-01 13:26:49 +0200

    update

---
+++ main.py
@@ -0,0 +1,41 @@
+from pygame import*
+from settings import WINDOW_WIDTH, WINDOW_HEIGHT,WHITE
+from keys import create_key_rects, draw_keys
+from sounds import load_sounds
+init()
+
+screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display.set_caption("Piano Game")
+
+sounds = load_sounds(KEYS)
+pressed_keys = set()
+key_rects = create_key_rects(7)
+keys_list = list(KEYS.keys())
+
+running = True
+
+while running:
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+        #клавіатура
+        if e.type == KEYDOWN:
+            pass
+        #миша
+        if e.type == MOUSEBUTTONDOWN:
+            pos = e.pos
+            for i, rect in enumerate(key_rects):
+                if rect.collidepoint(pos):
+                    sounds[keys_list[i]].play()
+                    pressed_keys.add(i)
+        if e.type == MOUSEBUTTONUP:
+            pos = e.pos
+            for i, rect in enumerate(key_rects):
+                if i in pressed_keys and rect.collidepoint(pos):
+                    pressed_keys.remove(i)
+
+    screen.fill(WHITE)
+    draw_keys(screen, key_rects, pressed_keys)
+    display.update()
+
+quit()
